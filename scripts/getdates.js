c572104290f517e3eb6727adc0f6ddf1547da3af
Ilegubo <ilegubo@gmail.com>
2026-03-18 03:45:05 +0000

    Dynamic footer elements for week 01

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,4 @@
 const year = new Date(document.lastModified);
 document.getElementById("lastModified").textContent = `last modified: ${year.toLocaleString()}`;
+const currentYear = new  Date().getFullYear();
+document.getElementById("copy-right-year").textContent = currentYear;
