6c487fc3dbf7435441a91c4afc4e79df4a18a307
Ilegubo <ilegubo@gmail.com>
2026-03-26 21:07:06 +0000

    W03 Assignment Large Screens

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,16 @@
 const lastModified = new Date(document.lastModified)
 document.getElementById('year').textContent = lastModified.getFullYear();
 document.getElementById('last-modified').textContent = `Last Modification: ${lastModified.toLocaleString()}`;
+
+let temperature = 10;
+let windSpeed = 10;
+const calculateWindChill = (temperature, windSpeed) => {
+    if (temperature <=10  && windSpeed>4.8){
+        return temperature * windSpeed;
+    }
+    return `Buggy Code`;
+}
+
+document.getElementById('temperature').textContent = temperature;
+document.getElementById('wind').textContent = `${windSpeed}`
+document.getElementById('wind-chill').textContent = `${calculateWindChill(temperature, windSpeed)}`;
